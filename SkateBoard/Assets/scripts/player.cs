8d028ce6a4889d6feb703d3511dbefa41c490d33
evliyabalik <evliyabalik@gmail.com>
2020-06-27 05:02:12 +0300

    evliya-27-06-2020

---
+++ SkateBoard/Assets/scripts/player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private charackterClass myPlayer;
+
+    [Header("Unit")] [SerializeField] private int unit=3;
+    void Start()
+    {
+        myPlayer=new charackterClass();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myPlayer.MouseLeftRight(this.gameObject.transform,unit);
+    }
+}
